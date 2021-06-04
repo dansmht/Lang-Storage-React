493a12c42246934638457df0752eaf6aaf91cf91
Daniil Smahtin <dansmht16@gmail.com>
2021-06-04 23:46:03 +0300

    add styles for container

--- src/components/shared/Container/Container.jsx
+++ src/components/shared/Container/Container.jsx
@@ -10,7 +10,7 @@ const Container = ({ children }) => (
 );
 
 Container.propTypes = {
-  children: PropTypes.elementType.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Container;
