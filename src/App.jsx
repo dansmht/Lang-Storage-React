ca3a8c459976282c61c32928b88aff745848eab9
dansmht <dansmht16@gmail.com>
2021-06-04 17:44:58 +0300

    fetch for current user when open app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchCurrentUser } from './redux/asyncActions/userActions';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, []);
+
   return (
     <div className='App'>
       Hello world
