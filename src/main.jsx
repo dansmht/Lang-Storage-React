209c8cc32b26b73ec1ec8564ba00bbb7e1b8a573
Daniil Smahtin <dansmht16@gmail.com>
2021-06-04 23:48:56 +0300

    move index.css and import variables and fonts to here

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-import './index.css';
+import './assets/styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
