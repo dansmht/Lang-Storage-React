9f3a5f9f3ce1340ded041f80598ef92ca8cf4be2
dansmht <dansmht16@gmail.com>
2021-06-14 18:17:25 +0300

    add interceptors for axios responses

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import history from '../utils/history';
+
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_SERVER_URL}/api`,
   headers: {
@@ -7,5 +9,18 @@ const api = axios.create({
   },
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    const errorCode = error.response.status;
+
+    if (errorCode === 403 || errorCode === 401) {
+      history.replace('/welcome');
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
