717b2fbedc5bd41a0e47e5a166e0831b9b33e027
dansmht <dansmht16@gmail.com>
2021-07-11 13:43:32 +0300

    create FilterButton component

---
+++ src/components/shared/FilterButton/FilterButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './FilterButton.module.scss';
+
+const FilterButton = () => {
+  return (
+    <div className={classes.FilterButton}>
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default FilterButton;
