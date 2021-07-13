ce641e03a390eec9d58827408836fa419c5acdf6
dansmht <dansmht16@gmail.com>
2021-07-14 00:23:53 +0300

    change logo from svg to divs and animate them by isLoading condition

--- src/components/shared/Logo/Logo.jsx
+++ src/components/shared/Logo/Logo.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Logo = () => (
-  <svg width='60' height='26' viewBox='0 0 60 26' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <path d='M26 13C26 5.8203 20.1797 0 13 0C5.8203 0 0 5.8203 0 13V26H13C20.1797 26 26 20.1797 26 13Z' fill='#FF7F50' />
-    <path d='M34 13C34 5.8203 39.8203 0 47 0C54.1797 0 60 5.8203 60 13V26H47C39.8203 26 34 20.1797 34 13Z' fill='#9AFA57' />
-  </svg>
+import classes from './Logo.module.scss';
+
+const Logo = ({ isLoading }) => (
+  <div
+    title={isLoading ? 'Loading...' : null}
+    className={classNames(classes.Logo, {
+      [`${classes.Loading}`]: isLoading,
+    })}
+  >
+    <div className={classes.LeftDrop} />
+    <div className={classes.RightDrop} />
+  </div>
 );
+
+Logo.propTypes = {
+  isLoading: PropTypes.bool,
+};
+
+Logo.defaultProps = {
+  isLoading: false,
+};
 
 export default Logo;
