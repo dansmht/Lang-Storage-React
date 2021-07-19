0cb610bae707092f340272e359b9fca545eea383
dansmht <dansmht16@gmail.com>
2021-07-19 21:58:10 +0300

    add user dropdown to the header and fix some header styles

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import Logo from '../../shared/Logo/Logo';
+import UserDropdown from './UserDropdown/UserDropdown';
 
 import classes from './Header.module.scss';
 
 const Header = () => {
   const { user, isLoading } = useSelector(state => state.user);
+
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+
+  const toggleUserDropdown = useCallback(() => {
+    setIsUserDropdownOpen((isOpen) => !isOpen);
+  }, []);
+
+  const closeUserDropdown = useCallback(() => {
+    setIsUserDropdownOpen(false);
+  }, []);
 
   return (
     <header className={classes.Header}>
@@ -35,9 +46,12 @@ const Header = () => {
                 <NavLink to='/collection'>My collection</NavLink>
               </li>
               <li className={classes.NavigationItem}>
-                <div className={classes.UserMenu} tabIndex={0}>
+                <div className={classes.UserMenu} tabIndex={0} onClick={toggleUserDropdown}>
                   <img src={user.picture} alt='User' />
                 </div>
+
+                {isUserDropdownOpen && <UserDropdown closeUserDropdown={closeUserDropdown} />}
+
               </li>
             </ul>
           )
